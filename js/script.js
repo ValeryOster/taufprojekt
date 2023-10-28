48775b82d6234343a7d75d565dcb5cbef82714c9
oster <oster_valera@hotmail.com>
2023-10-28 18:04:37 +0200

    Project Update: js html and backend

--- js/script.js
+++ js/script.js
@@ -60,41 +60,73 @@ function setTaufpatin(myVar) {
     document.getElementById('geburstortTaufpatin').required = myVar;
 }
 
-function loadData(searchData) {
+function loadData(searchName) {
+    let tableBody = document.getElementById("resultSearchTable");
+    for (var i = 1; i < tableBody.rows.length;) {
+        tableBody.deleteRow(i);
+    }
 
     let httpRequest = new XMLHttpRequest();
     httpRequest.open('POST', 'php/db/search.php', true);
     httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    httpRequest.onload = function(){
-        console.log(this.responseText);
+
+    function dateFormating(date) {
+        let splice = date.split("-");
+        return splice[2] + "." + splice[1] + "." + splice[0];
     }
-    httpRequest.send("search="+searchData);
+
+    function dateFormatingAmis(date) {
+        let splice = date.split(".");
+        return splice[2] + "-" + splice[1] + "-" + splice[0];
+    }
+
+
+    function adjustToField(str) {
+        var myModal = new bootstrap.Modal(document.getElementById('pattronNameAdjustingModal'), {keyboard: false});
+        document.getElementById('patronenNameField').value = str;
+        myModal.show();
+        myModal.addEventListener('hidden.bs.modal', function (event) {
+            console.log("am hier")
+            document.getElementById('feiertagNamestag').value = document.getElementById('patronenNameField').value;
+        })
+    }
+
+    httpRequest.onload = function () {
+
+        let newResp = this.responseText.replaceAll(String.fromCharCode(92) + "u2020", "");
+        try {
+            let resultTexting = JSON.parse(newResp);
+            if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+
+                let iterator = 1;
+                if (resultTexting.length > 0) {
+                    resultTexting.forEach(rsText => {
+                        let htmlTableRowElement = tableBody.insertRow(iterator);
+                        htmlTableRowElement.insertCell(0).innerHTML = rsText.name.charAt(0) + rsText.name.slice(1).toLowerCase();
+                        htmlTableRowElement.insertCell(1).innerHTML = dateFormating(rsText.date);
+                        htmlTableRowElement.insertCell(2).innerHTML = rsText.description;
+                        htmlTableRowElement.addEventListener("click", function () {
+                            document.getElementById('sacramenteDatum').value = dateFormatingAmis(this.querySelectorAll("*")[1].innerHTML.trim());
+                            adjustToField(this.querySelectorAll("*")[2].innerHTML.trim());
+                        });
+                        iterator++;
+                    });
+
+
+                } else {
+                    html += '<tr><td colspan="3" class="text-center">No Data Found</td></tr>';
+                }
+
+            }
+        } catch (e) {
+            console.log("Error: ", e.message);
+        }
+    }
+
+    httpRequest.send("search=" + searchName);
+
 
     httpRequest.onreadystatechange = function () {
-        if(httpRequest.readyState == 4 && httpRequest.status == 200)
-        {
-            let response = JSON.parse(httpRequest.responseText);
-            let html = '';
-            let serial_no = 1;
-            if(response.length > 0)
-            {
-                for(var count = 0; count < response.length; count++)
-                {
-                    html += '<tr>';
-                    html += '<td>'+serial_no+'</td>';
-                    html += '<td>'+response[count].post_title+'</td>';
-                    html += '<td>'+response[count].post_description+'</td>';
-                    html += '</tr>';
-                    serial_no++;
-                }
-            }
-            else
-            {
-                html += '<tr><td colspan="3" class="text-center">No Data Found</td></tr>';
-            }
-            document.getElementById('post_data').innerHTML = html;
-            document.getElementById('total_data').innerHTML = response.length;
-        }
     };
 
 }
@@ -102,10 +134,34 @@ function loadData(searchData) {
 function search() {
     let searchName = document.getElementById('searchModalName').value;
     if (searchName) {
-        console.log(searchName);
         loadData(searchName);
-    }else {
+    } else {
         let searchDate = document.getElementById('searchModalDate').value;
-        console.log(searchDate);
     }
 }
+
+var date = new Date();
+$(function () {
+    $(".searchModalDate").datepicker({language: "de"});
+    $("#searchModalDate").datepicker({
+        prevText: '&#x3c;zurück', prevStatus: '',
+        prevJumpText: '&#x3c;&#x3c;', prevJumpStatus: '',
+        nextText: 'Vor&#x3e;', nextStatus: '',
+        nextJumpText: '&#x3e;&#x3e;', nextJumpStatus: '',
+        currentText: 'heute', currentStatus: '',
+        todayText: 'heute', todayStatus: '',
+        clearText: '-', clearStatus: '',
+        closeText: 'schließen', closeStatus: '',
+        monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+            'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+        monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+        dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+        dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+        dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+        dateFormat: "dd MM",
+        changeMonth: true,
+        minDate: new Date(date.getFullYear(), 0, 1),
+        maxDate: new Date(date.getFullYear(), 12, 31),
+    });
+});
--- php/db/search.php
+++ php/db/search.php
@@ -3,21 +3,38 @@ include "dbconnection.php";
 
 
 $search = $_POST['search'];
-if (isset( $search )) {
-	$Name = $search;
-	$Query = "SELECT * FROM heiligen WHERE name LIKE '%$Name%'";
-	$ExecQuery = MySQLi_query($conn, $Query);
-	echo '
-<ul>
-   ';
-	while ($Result = MySQLi_fetch_array($ExecQuery)) {
-		?>
-		<li onclick='fill("<?php echo $Result['name']; ?>")'>
-			<a>
-				<?php echo $Result['name']; ?>
-		</li></a>
-		<?php
-	}}
-?>
-</ul>
-?>
+if ( isset( $search ) ) {
+	$Name      = $search;
+	$Query     = "SELECT * FROM heiligen WHERE name LIKE '%$Name%'";
+	$ExecQuery = MySQLi_query( $conn, $Query );
+
+	$finalResult = array();
+	$i = 0;
+	if ( $ExecQuery->num_rows > 0 ) {
+		while ( $Result = MySQLi_fetch_array( $ExecQuery ) ) {
+			$finalResult[ $i ]['name'] = $Result['name'];
+			$finalResult[ $i ]['date'] = $Result['date'];
+			$finalResult[ $i ]['ehre'] = $Result['ehre'];
+			$finalResult[ $i ]['description'] = $Result['description'];
+			$i ++;
+		}
+	}
+
+	$json_encode = json_encode( $finalResult );
+	echo $json_encode;
+	die();
+}
+
+function sonderzeichen($string)
+{
+	$string = str_replace("ä", "ae", $string);
+	$string = str_replace("ü", "ue", $string);
+	$string = str_replace("ö", "oe", $string);
+	$string = str_replace("Ä", "Ae", $string);
+	$string = str_replace("Ü", "Ue", $string);
+	$string = str_replace("Ö", "Oe", $string);
+	$string = str_replace("ß", "ss", $string);
+	$string = str_replace("´", "", $string);
+	return $string;
+}
+
