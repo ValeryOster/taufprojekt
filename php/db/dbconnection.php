9897c5a983d81c7382c7c0793384dcb538d2f599
oster <oster_valera@hotmail.com>
2023-11-24 21:41:41 +0100

    Project Update:  php html

--- php/db/dbconnection.php
+++ php/db/dbconnection.php
@@ -8,16 +8,3 @@ $dbname = "heiligen";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname );
 
-// Check connection
-//if ($conn->connect_error) {
-//  die("Connection failed: " . $conn->connect_error);
-//}
-//echo "Connected successfully";
-//
-//if($result = $conn->query('SHOW TABLES')){
-//    while($row = $conn->fetch_array($result)){
-//        $tables[] = $row[0];
-//        echo $tables;
-//    }
-//}
-//echo "\nEnds";
